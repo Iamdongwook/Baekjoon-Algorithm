199f89a046bf805e0cca979714eeaceb14a361df
Dongwook Kim <79130141+Iamdongwook@users.noreply.github.com>
2021-02-23 00:04:31 +0900

    시뮬레이션

--- 삼성 SW 역량 테스트/14503번: 로봇 청소기/main.cpp
+++ 삼성 SW 역량 테스트/14503번: 로봇 청소기/main.cpp
@@ -8,6 +8,7 @@ int r,c,d;
 int Map[MAX][MAX];
 int clean=0;
 
+//북 동 남 서 
 int directX[] = {0,1,0,-1};
 int directY[] = {-1,0,1,0};
 
@@ -23,18 +24,22 @@ int main() {
   } 
 
   while(1) {
-
+    
+    //현재 위치를 청소한다.
     if(Map[r][c]==0) {
       Map[r][c]=2;
       clean++;
     }
 
     int nextStep = 0;
+    
+    //왼쪽부터 차례대로 탐색
     for(int i=0; i<4; i++) {
       d = (d + 3) % 4;
       int nextX = directX[d] + c;
       int nextY = directY[d] + r;
-
+      
+      //청소할 공간이 있으면 한칸 전진 
       if(Map[nextY][nextX]==0) {
         c = nextX;
         r = nextY;
@@ -42,11 +47,13 @@ int main() {
         break;
       }
     }
-
+   
+    //4가지 방향 check 
     if(nextStep==1) {
         continue;
     }
     
+    //후진 && d는 그대로 유지
     int backStep = (d + 2) % 4;
     c = directX[backStep] + c;
     r = directY[backStep] + r;
