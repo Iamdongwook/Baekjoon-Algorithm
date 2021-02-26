9d0a5ea3f7e23ddce6f35b3e268a284fa7ed9545
Dongwook Kim <79130141+Iamdongwook@users.noreply.github.com>
2021-02-26 16:07:07 +0900

    수정 필요

--- 삼성 SW 역량 테스트/14501: 퇴사/main.cpp
+++ 삼성 SW 역량 테스트/14501: 퇴사/main.cpp
@@ -20,12 +20,13 @@ void dfs(int today, int sum) {
 
     //두가지 경우가 있다.
     //이해가 안가서 한참 헤멨다.
-  
-    if (today + day[today] < N + 1) { // 일을 함
+    //아직 까지 <= 부호를 모르겠음. 단순히 < 아님? 휴......
+ 
+    if (today + day[today] <= N + 1) { // 일을 함
         dfs(today + day[today], sum + money[today]);
     }
  
-    if (today + 1 < N + 1) { // 일을 안하고, 다음 날로 감
+    if (today + 1 <= N + 1) { // 일을 안하고, 다음 날로 감
         dfs(today + 1, sum);
     }
 }
