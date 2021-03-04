1ba23f63c4bf8849413579c26ca187974e280fb3
Dongwook Kim <79130141+Iamdongwook@users.noreply.github.com>
2021-03-05 01:11:23 +0900

    브루트포스

---
+++ Brute-force/1436번: 영화감독 숌/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int N;
+
+void solution()
+{
+    int i = 666;
+    int series = 0;
+    string target;
+
+    while(1)
+    {
+        target = to_string(i);
+        for(int i = 0; i < target.length(); i++)
+            if(target[i] == '6' && target[i+1] == '6' && target[i+2] == '6')
+            {
+                series++;
+                break;
+            }
+
+        if(series == N) {
+          cout << i;
+          break;
+        } else {
+          i++;
+        }   
+    }
+}
+int main() {
+    
+    cin >> N;
+
+    solution();
+}
